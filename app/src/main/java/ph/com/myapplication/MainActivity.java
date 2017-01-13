6729a1d8fe6fe06e4ce818cbc31dc30defc59ff8
penghao1 <395937009@qq.com>
2017-01-13 10:51:12 +0800

    文字控件修改

--- app/src/main/java/ph/com/myapplication/MainActivity.java
+++ app/src/main/java/ph/com/myapplication/MainActivity.java
@@ -2,14 +2,18 @@ package ph.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text_vw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "这是一个新加代码", Toast.LENGTH_SHORT).show();
+        text_vw.setText("修改一下代码而已");
+
     }
 }
